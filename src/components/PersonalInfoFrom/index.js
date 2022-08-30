fb25927603e4196b19846069cac5b47bc468e578
alif33 <alifhasan332@gmail.com>
2022-08-30 22:42:58 +0600

    ...

--- src/components/PersonalInfoFrom/index.js
+++ src/components/PersonalInfoFrom/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
+import { updateData } from '../../__lib__/helpers/HttpService';
 
 const PersonalInfoForm = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const { users } = useSelector(state => state);
     const { __u__ } = users;
-    const onSubmit = data => console.log(data);
+
+    const onSubmit = data => {
+        const _data = new FormData();
+        _data.append("firstName", data.firstName);
+        _data.append("lastName", data.lastName);
+        _data.append("phoneNumber", data.phoneNumber);
+        // _data.append("images", data.productImage[0]);
+        // _data.append("images", data.studentImage[0]);
+        updateData(`/${ __u__.role==="AUTHOR"?"author": "student" }/profile`, _data, __u__.token)
+        .then(res=>{
+            console.log(res);
+        })
+        console.log(__u__);
+    };
     return (
         <From onSubmit={handleSubmit(onSubmit)}>
 
@@ -17,20 +31,18 @@ const PersonalInfoForm = () => {
                     />
                     <div className={errors.firstName ? "inputDiv active" : "inputDiv "}>
                         <input
-                            {...register("bankName", { required: true })}
                             placeholder="First Name"
                             defaultValue={__u__.info.firstName}
                             {...register("firstName", { required: true })}
                         />
-                        {errors.bankName && <span>bankName is required</span>}
+                        {errors.firstName && <span>First Name is required</span>}
                     </div>
                 </div>
                 <div className="inputsConatiner">
                     <img src='/img/icon/user.svg' className="ledtIcon" alt=""
                     />
-                    <div className={errors.firstName ? "inputDiv active" : "inputDiv "}>
+                    <div className={errors.lastName ? "inputDiv active" : "inputDiv "}>
                         <input
-                            {...register("bankName", { required: true })}
                             placeholder="Last Name"
                             defaultValue={__u__.info.lastName}
                             {...register("lastName", {
@@ -38,7 +50,7 @@ const PersonalInfoForm = () => {
                             })
                             }
                         />
-                        {errors.bankName && <span>bankName is required</span>}
+                        {errors.lastName && <span>Last Name is required</span>}
                     </div>
                 </div>
             </div>
@@ -46,27 +58,24 @@ const PersonalInfoForm = () => {
                 <div className="inputsConatiner">
                     <img src='/img/icon/mail.svg' className="ledtIcon" alt=""
                     />
-                    <div className={errors.firstName ? "inputDiv active" : "inputDiv "}>
+                    <div className="inputDiv">
                         <input
-                            {...register("bankName", { required: true })}
                             placeholder="Email"
+                            disabled={ true }
                             defaultValue={__u__.info.email}
-                            {...register("email", { required: true })}
                         />
-                        {errors.bankName && <span>bankName is required</span>}
                     </div>
                 </div>
                 <div className="inputsConatiner">
                     <img src='/img/icon/contact.svg' className="ledtIcon" alt=""
                     />
-                    <div className={errors.firstName ? "inputDiv active" : "inputDiv "}>
+                    <div className={errors.phoneNumber ? "inputDiv active" : "inputDiv "}>
                         <input
-                            {...register("bankName", { required: true })}
                             placeholder="Phone Number"
                             defaultValue={__u__.info.phoneNumber}
                             {...register("phoneNumber", { required: true })}
                         />
-                        {errors.bankName && <span>bankName is required</span>}
+                        {errors.phoneNumber && <span>Phone Number is required</span>}
                     </div>
                 </div>
             </div>
