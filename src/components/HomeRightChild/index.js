f41450914d061e436e4b4a2f280fa7475a61fecb
developerYeasin <yea13sin@gmail.com>
2022-08-31 03:30:01 +0600

    home page animation

--- src/components/HomeRightChild/index.js
+++ src/components/HomeRightChild/index.js
@@ -4,24 +4,10 @@ import styled from 'styled-components';
 const HomeRightChild = () => {
   return (
     <Container>
-      <div className="firstImagesColumn">
-        <img src="/img/icon/cycle.svg" style={{ width: 160, height: 160, marginTop: 20 }} alt="" />
-
-        <img src="/img/icon/bag.svg" style={{ width: 160, height: 160, marginTop: 20 }} alt="" />
-      </div>
-      <div className="secondImagesColumn">
-        <img
-          src="/img/icon/books.svg"
-          style={{ width: 160, height: 160, marginBottom: 20 }}
-          alt=""
-        />
-
-        <img
-          src='/img/icon/table.svg'
-          style={{ width: 160, height: 160, marginBottom: 20 }}
-          alt=""
-        />
-      </div>
+      <img className="header-img1" src="/img/icon/cycle.svg" alt="" />
+      <img className="header-img2" src="/img/icon/bag.svg" alt="" />
+      <img className="header-img3" src="/img/icon/books.svg" alt="" />
+      <img className="header-img4" src='/img/icon/table.svg' alt="" />
     </Container>
   );
 };
@@ -31,55 +17,12 @@ export default HomeRightChild;
 
 const Container = styled.div`
   background-color: white;
-  /* height: 80vh; */
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  .firstImagesColumn {
-    width: 30%;
-    height: 60%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-top: 20;
-  }
-  .secondImagesColumn {
-    width: 30%;
-    height: 60%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 0 0 20px 40px;
-  }
-  h2 {
-    text-align: center;
-  }
-  .imgsDiv {
-    //background-color: aqua;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    margin-top: 20;
-    justify-content: space-around;
-  }
-  .imgDiv {
-    width: 15%;
-    background-color: antiquewhite;
-    height: 0%;
-    padding-top: 200;
-  }
-  img {
-    height: 100%;
-    width: 100%;
-    @media (max-width: 576px){
-      width: 115px !important;
-    height: auto;
+  height: 382px;
+  width: 530px;
+  position: relative;
 
-    }
+  img {
+    margin-top: 20px;
+    position: absolute;
   }
 `;
