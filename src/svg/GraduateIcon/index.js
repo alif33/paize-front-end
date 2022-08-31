14df0314bf93e60e277e00bf9e85933bbb69107d
developerYeasin <yea13sin@gmail.com>
2022-09-01 03:49:09 +0600

    svg icon making

--- src/svg/GraduateIcon/index.js
+++ src/svg/GraduateIcon/index.js
@@ -5,7 +5,7 @@ const GraduateIcon = () => {
         <svg width="36" height="30" viewBox="0 0 36 30" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect width="36" height="30" fill="#E5E5E5"/>
         <rect width="1512" height="982" transform="translate(-71 -377)" fill="white"/>
-        <path d="M33.625 18.125V10L18 1.875L2.375 10L18 18.125L26.125 14.375V23.125C26.125 25.625 22.375 28.125 18 28.125C13.625 28.125 9.875 25.625 9.875 23.125V14.375" stroke="#2291F1" strokeWidth="2.8125" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M33.625 18.125V10L18 1.875L2.375 10L18 18.125L26.125 14.375V23.125C26.125 25.625 22.375 28.125 18 28.125C13.625 28.125 9.875 25.625 9.875 23.125V14.375" stroke="#2291F1" strokeWidth="2.8125" strokeLinecap="round" stroke-linejoin="round"/>
         <rect x="-3614.5" y="-613.5" width="7237" height="1461" stroke="black" strokeWidth="27"/>
         </svg>
         
--- src/svg/SearchIcon/index.js
+++ src/svg/SearchIcon/index.js
@@ -12,8 +12,8 @@ const SearchIocn = () => {
             <path d="M-440 -51H1070V93H-440V-51Z" fill="white" />
             <path d="M1070 92H-440V94H1070V92Z" fill="#DADDE1" mask="url(#path-1-outside-1_0_1)" />
             <rect x="-41.0005" y="-15.5" width="648.141" height="72" rx="10.5" fill="#DADDE1" fillOpacity="0.4" stroke="#DADDE1" />
-            <path d="M18.7527 31.6667C26.1068 31.6667 32.0684 25.6971 32.0684 18.3333C32.0684 10.9695 26.1068 5 18.7527 5C11.3987 5 5.43701 10.9695 5.43701 18.3333C5.43701 25.6971 11.3987 31.6667 18.7527 31.6667Z" stroke="#0E3746" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M35.3976 35L28.1572 27.75" stroke="#0E3746" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M18.7527 31.6667C26.1068 31.6667 32.0684 25.6971 32.0684 18.3333C32.0684 10.9695 26.1068 5 18.7527 5C11.3987 5 5.43701 10.9695 5.43701 18.3333C5.43701 25.6971 11.3987 31.6667 18.7527 31.6667Z" stroke="#0E3746" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round" />
+            <path d="M35.3976 35L28.1572 27.75" stroke="#0E3746" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round" />
             <rect x="-9131.5" y="-361.5" width="10661" height="2037" stroke="black" strokeWidth="27" />
         </svg>
     );
