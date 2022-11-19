5ed3ac105ca6e0e4f46259e34bb50e916b063793
alif33 <alifhasan332@gmail.com>
2022-11-19 20:31:43 +0600

    ...

--- src/__lib__/helpers/Filter.js
+++ src/__lib__/helpers/Filter.js
@@ -5,3 +5,10 @@ export const sortByStatus = (arr) =>{
         rejected: arr.filter(item => item.status === 'REJECTED')
     }  
 }
+
+export const filterByStatus = (arr) =>{
+    return {
+        pending: arr.filter(item => item.status === 'PENDING'),
+        completed: arr.filter(item => item.status === 'COMPLETED')    
+    }  
+}
